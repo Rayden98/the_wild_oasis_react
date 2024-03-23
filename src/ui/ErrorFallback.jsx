6ad604794f8826c512894718c415e7802f361805
Enmanuel Cruz <rember98cjs@gmail.com>
2024-03-23 13:29:29 -0600

    finishing

--- src/ui/ErrorFallback.jsx
+++ src/ui/ErrorFallback.jsx
@@ -1,4 +1,9 @@
+/* eslint-disable */
+import PropTypes from "prop-types";
 import styled from "styled-components";
+import Heading from "./Heading";
+import GlobalStyles from "../styles/GlobalStyles";
+import Button from "./Button";
 
 const StyledErrorFallback = styled.main`
   height: 100vh;
@@ -29,3 +34,26 @@ const Box = styled.div`
     color: var(--color-grey-500);
   }
 `;
+
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return (
+    <>
+      <GlobalStyles />
+      <StyledErrorFallback>
+        <Box>
+          <Heading as="h1">Something went wrong </Heading>
+          <p>{error.message}</p>
+          <Button size="large" onClick={resetErrorBoundary}>
+            Try again
+          </Button>
+        </Box>
+      </StyledErrorFallback>
+    </>
+  );
+}
+
+ErrorFallback.propTypes = {
+  error: PropTypes.string.isRequired, // You can adjust the prop type as per your requirement
+};
+
+export default ErrorFallback;
